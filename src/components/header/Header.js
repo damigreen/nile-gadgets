1e690f12d1c712b64425d760accfff62003dc4b7
Damilola Faseun <fashfired@gmail.com>
2020-11-10 09:01:38 +0100

    Created a header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from '../navigation/';
+import Hero from '../hero/';
+import './Header.scss';
+
+function Header() {
+
+  return (
+    <header id="header">
+      <div className="row">
+        <h1>Space Gadgets</h1>
+        <Navigation />
+      </div>
+    </header>
+  )
+}
+
+export default Header;
