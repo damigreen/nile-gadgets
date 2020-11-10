76f1143e141a5cc502bdd38679348b98e31dabe9
Damilola Faseun <fashfired@gmail.com>
2020-11-10 09:02:09 +0100

    Added a hero component

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import heroImage from '../../images/hero/HeroImage.png';
+
+function Hero() {
+
+  return (
+    <div>
+      <img alt="the Hero image" src={heroImage} />
+    </div>
+  )
+}
+
+export default Hero;
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navigation() {
+
+  return (
+    <nav id="navigation" className="">
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT US</a></li>
+        <li><a href="#">CATEGORIES</a></li>
+        <li><a href="#">CATEGORIES</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation;
