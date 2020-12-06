32eea20d9da5842f9f43e2ff08c99c32b823efe6
Damilola Faseun <fashfired@gmail.com>
2020-12-06 14:37:15 +0100

    anchored navigation to home

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,10 +5,12 @@ function Navigation() {
   return (
     <nav id="navigation" className="">
       <ul>
-        <li><a href="#">HOME</a></li>
+        {/* <li><a href="#">HOME</a></li> */}
         <li><a href="#">ABOUT US</a></li>
         <li><a href="#">CATEGORIES</a></li>
-        <li><a href="#">CATEGORIES</a></li>
+        <li><a href="#">USER</a></li>
+        <li><a href="#">BUY</a></li>
+        <li><a href="#">SELL</a></li>
       </ul>
     </nav>
   )
